a8fb56324eb781e2ea0ef0537bfbd7306d80063e
Kenneth Love <kenneth@gigantuan.net>
2014-04-01 14:06:20 -0700

    that's stupid

--- survivalguide/wsgi.py
+++ survivalguide/wsgi.py
@@ -11,9 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survivalguide.settings")
-# application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(get_wsgi_application())
